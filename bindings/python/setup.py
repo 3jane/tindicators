55623e92df7a5f0273bdbd68aa12e9c36df8b74b
Anatoly <elengar17@gmail.com>
2019-05-15 18:50:59 +0300

    Moved tests into the package and added tox, restructured requirements in setup.py, removed requirements.txt

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -4,6 +4,27 @@ import sys
 sys.path.insert(0, '../..')
 from version import version
 
+
+INSTALL_REQUIREMENTS = [
+    "numpy"
+]
+
+SETUP_REQUIREMENTS = [
+    "pytest-runner"
+]
+
+TESTS_REQUIREMENTS = [
+    "tox",
+    "tox-wheel",
+    "pytest",
+    "pandas"
+]
+
+DEV_REQUIREMENTS = [
+
+]
+
+
 setuptools.setup(
     name="tulipindicators",
     version=version,
@@ -11,7 +32,12 @@ setuptools.setup(
     author_email="ilya.p@rcdb.io",
     description="Python wrapper for Tulip Indicators",
     packages=setuptools.find_packages(),
-    install_requires=["numpy"],
+    setup_requires=SETUP_REQUIREMENTS,
+    tests_require=TESTS_REQUIREMENTS,
+    install_requires=INSTALL_REQUIREMENTS,
+    extras_require={
+        'dev': tuple(TESTS_REQUIREMENTS + DEV_REQUIREMENTS)
+    },
     classifiers=["Programming Language :: Python :: 3", "Operating System :: OS Independent"],
     package_data={"tulipindicators": ["libindicators.so", "indicators.dll"]}
 )
