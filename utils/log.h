680897716662c177f1f21fb9c66efc84654ad7ea
Ilya Pikulin <ilya.pikulin@gmail.com>
2019-07-01 23:43:07 +0300

    log: cast argument to double

--- utils/log.h
+++ utils/log.h
@@ -1,2 +1,2 @@
 #include <stdio.h>
-#define LOG(what) fprintf(stderr, "%s %s = %f\n", __FUNCTION__, #what, what);
+#define LOG(what) fprintf(stderr, "%s %s = %f\n", __FUNCTION__, #what, (double)(what));
