87744e4e073ad44fea46cd32f9740b1a5907635a
Ilya Pikulin <ilya.pikulin@gmail.com>
2019-07-02 03:12:46 +0300

    Gaussian Filter - 2 Poles

--- benchmark2.c
+++ benchmark2.c
@@ -242,6 +242,10 @@ void gf1_option_setter(double period, double* options) {
     options[0] = MIN(period*2., 40.);
 }
 
+void gf2_option_setter(double period, double* options) {
+    options[0] = MIN(period*2, 40);
+}
+
 void bench(const ti_indicator_info *info) {
     printf("Running: %s ", info->name);
     fflush(stdout);
@@ -280,6 +284,7 @@ void bench(const ti_indicator_info *info) {
     if (strcmp(info->name, "bf2") == 0) { options_setter = bf23_option_setter; }
     if (strcmp(info->name, "bf3") == 0) { options_setter = bf23_option_setter; }
     if (strcmp(info->name, "gf1") == 0) { options_setter = gf1_option_setter; }
+    if (strcmp(info->name, "gf2") == 0) { options_setter = gf2_option_setter; }
 
     static TI_REAL *inputs[TI_MAXINDPARAMS];
     for (int i = 0; i < info->inputs; ++i) {
