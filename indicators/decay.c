8ebc6bb11ac43236dd71061304ae949bc6415aa6
Ilya Pikulin <ilya.pikulin@gmail.com>
2019-07-20 08:15:15 +0300

    decay: fix unchecked memory access

--- indicators/decay.c
+++ indicators/decay.c
@@ -37,14 +37,14 @@ int ti_decay(int size, TI_REAL const *const *inputs, TI_REAL const *options, TI_
     const int period = (int)options[0];
     const TI_REAL scale = 1.0 / period;
 
-    *output++ = input[0];
-
-    int i;
-    for (i = 1; i < size; ++i) {
+    int i = 0;
+    for (; i < 1 && i < size; ++i) {
+        *output++ = input[0];
+    }
+    for (; i < size; ++i) {
         TI_REAL d = output[-1] - scale;
         *output++ = input[i] > d ? input[i] : d;
     }
-
 
     return TI_OKAY;
 }
