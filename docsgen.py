45780993e5c8f7c1eb768ad068ba7edde0a11b40
Ilya Pikulin <ilya.pikulin@gmail.com>
2021-02-14 20:12:33 +0300

    docsgen: fix links to the implementations

--- docsgen.py
+++ docsgen.py
@@ -67,7 +67,7 @@ for indicator in indicators.items():
 	outfile.write(f'''
 #### {elab_name}
 *Source:* {source} <br/>
-*Implementation:* [{filename}](https://github.com/3jane/tindicators/indicators/{filename}) <br/>
+*Implementation:* [{filename}](https://github.com/3jane/tindicators/indicators/tree/master/{filename}) <br/>
 *Signature:*
 
 ```python
